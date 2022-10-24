ab7d8821890060037a9164d5976ce9c370158c2c
Fujimoto Kentaro <f.kentaro113@gmail.com>
2022-10-24 23:29:55 +0900

    add formatter

--- weekly_report/main.py
+++ weekly_report/main.py
@@ -2,6 +2,11 @@ from git import *    #git.~で使用する必要がないようにgit 以下をi
 # url = 'https://gitlab.tokyo.optim.co.jp/optim-store/tools/optim_store_recovery'
 # url = 'https://gitlab.tokyo.optim.co.jp/optim-store/tools/bpcc-text-checker'
 
+from enum import Enum
+class RepoType(Enum):
+    RECOVERY = 1
+    CREATE_TEXT = 2
+
 # search_from_commits(made_list, "Fujisan", 144)
 def search_from_commits(commit_list, author_name, project_number):
     my_commits = []
@@ -25,6 +30,18 @@ def slice_commit(my_commits):
     print(commit_number_list)
     return commit_number_list
 
+
+def formatter(commit_number_list, repo_type = RepoType.RECOVERY):
+    for a in commit_number_list:
+        if repo_type == RepoType.RECOVERY:
+            a = a + 'のリカバリー'
+            print(a)
+        elif repo_type == RepoType.CREATE_TEXT:
+            a = a + 'のテキスト作成'
+            print(a)
+        else:
+            print('登録されていないリポジトリ')
+
 # input: repository url
 # output: all commit (restricted commit)
 def get_commits_from_path(repo_path):
@@ -43,7 +60,10 @@ if __name__ == '__main__': # おまじない
     for path in git_repo_paths:
         commit_list = get_commits_from_path(path)
         my_commits = search_from_commits(commit_list, "Fujimoto Kentaro", 1)
-        slice_commit(my_commits)
+        commit_number_list = slice_commit(my_commits)
+        formatter(commit_number_list)
+        formatter(commit_number_list, RepoType.CREATE_TEXT)
+        formatter(commit_number_list, 123)
         
 
         
