d3032bffb0f64121bed6df738be775975fcdecb9
DirectKidman <trumpet.programing525@gmail.com>
2022-11-07 16:12:00 +0900

    remove .DS_Store

--- playground.py
+++ playground.py
@@ -1,2 +1,10 @@
-with open('write_test.txt','w') as f:
-    f.write('Hello World')
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+
+options = Options()
+options.add_argument("user-data-dir=" + '/Users/seijimurai/Library/Application Support/Google/Chrome/Default')
+
+driver = webdriver.Chrome(options=options)
+
+driver.get("https://www.amazon.co.jp/")
